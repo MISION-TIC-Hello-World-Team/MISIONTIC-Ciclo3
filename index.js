3873e88c14322d0126f713d9886e3031920cfb4a
oscarnicolas <oscarnicolasmozo@gmail.com>
2021-10-20 19:00:03 +0200

    backend

---
+++ index.js
@@ -0,0 +1,53 @@
+import Express from "express";
+import { MongoClient } from "mongodb";
+
+const app = Express();
+app.use(Express.json());
+const stringConexion = "mongodb+srv://oscarnicolas:misiontic@cluster0.js2zb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(stringConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+app.get("/productMaster", (req, res) => {
+    baseDeDatos.collection("producto").find().limit(50).toArray((err,result)=>{
+        if(err){
+            res.status(500).send("error consultando productos")
+        }else{
+            res.json(result);
+        }
+    });
+});
+
+
+app.post("/productMaster/nuevo", (req, res) => {
+    const datosProductos = req.body;
+    console.log(datosProductos);
+    baseDeDatos.collection("producto").insertOne(datosProductos, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+          } else {
+            console.log(result);
+            res.sendStatus(200);
+          }
+    });
+});
+
+
+let baseDeDatos;
+const main = () => {
+    client.connect((err, db) => {
+      if (err) {
+        console.error('Error conectando a la base de datos');
+        return 'error';
+      }
+      baseDeDatos = db.db('concesionario');
+      console.log('baseDeDatos exitosa');
+      return app.listen(5000, () => {
+        console.log('escuchando puerto 5000');
+      });
+    });
+  };
+  
+  main();

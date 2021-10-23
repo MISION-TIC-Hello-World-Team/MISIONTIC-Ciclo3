ab46bb1f09657ef64f0e30d3a07afd6fd49912ec
oscarnicolas <oscarnicolasmozo@gmail.com>
2021-10-24 00:13:02 +0200

    backend-insomnia working for users

--- users.js
+++ users.js
@@ -2,8 +2,6 @@ import Express from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import { stringConexion } from "./stringConexion.js";
 import cors from "cors";
-import { mainSales } from "./sales.js";
-import { mainUsers } from "./users.js";
 
 
 const app = Express();
@@ -14,10 +12,10 @@ const client = new MongoClient(stringConexion, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-app.get("/productMaster", (req, res) => {
-    baseDeDatos.collection("producto").find().limit(50).toArray((err, result) => {
+app.get("/userMaster", (req, res) => {
+    baseDeDatos.collection("usuarios").find().limit(50).toArray((err, result) => {
         if (err) {
-            res.status(500).send("error consultando productos")
+            res.status(500).send("error consultando usuarios")
         } else {
             res.json(result);
         }
@@ -25,10 +23,10 @@ app.get("/productMaster", (req, res) => {
 });
 
 
-app.post("/productMaster/nuevo", (req, res) => {
-    const datosProductos = req.body;
-    console.log(datosProductos);
-    baseDeDatos.collection("producto").insertOne(datosProductos, (err, result) => {
+app.post("/userMaster/nuevo", (req, res) => {
+    const datosUsuarios = req.body;
+    console.log(datosUsuarios);
+    baseDeDatos.collection("usuarios").insertOne(datosUsuarios, (err, result) => {
         if (err) {
             console.error(err);
             res.sendStatus(500);
@@ -39,16 +37,16 @@ app.post("/productMaster/nuevo", (req, res) => {
     });
 });
 
-app.patch('/productMaster/editar', (req, res) => {
+app.patch('/userMaster/editar', (req, res) => {
     const edicion = req.body;
-    const filtroProducto = {_id:  new ObjectId(edicion.id)};
+    const filtroUsuario = {_id:  new ObjectId(edicion.id)};
     delete edicion.id;
     const operacion = {
         $set:edicion,
     };
-    baseDeDatos.collection("producto").updateOne(filtroProducto, operacion, { upsert: true, returnOriginal: true },(err,result)=>{
+    baseDeDatos.collection("usuarios").updateOne(filtroUsuario, operacion, { upsert: true, returnOriginal: true },(err,result)=>{
         if (err){
-            console.error("error editando producto",err);
+            console.error("error editando usuario",err);
             res.sendStatus(500);
         }else{
             console.log("actualizado con exito");
@@ -56,9 +54,9 @@ app.patch('/productMaster/editar', (req, res) => {
         }
     });
 });
-app.delete('/productMaster/eliminar', (req, res) => {
-    const filtroProducto = { _id: new ObjectId(req.body.id) };
-    baseDeDatos.collection('producto').deleteOne(filtroProducto, (err, result) => {
+app.delete('/userMaster/eliminar', (req, res) => {
+    const filtroUsuario = { _id: new ObjectId(req.body.id) };
+    baseDeDatos.collection('usuarios').deleteOne(filtroUsuario, (err, result) => {
         if (err) {
             console.error(err);
             res.sendStatus(500);
@@ -69,20 +67,19 @@ app.delete('/productMaster/eliminar', (req, res) => {
 });
 
 let baseDeDatos;
-const main = () => {
+
+export const mainUsers= () => {
     client.connect((err, db) => {
         if (err) {
             console.error('Error conectando a la base de datos');
             return 'error';
         }
         baseDeDatos = db.db('concesionario');
-        console.log('baseDeDatos exitosa');
-        return app.listen(5000, () => {
-            console.log('escuchando puerto 5000');
+        console.log('baseDeDatos sales exitosa');
+        return app.listen(6000, () => {
+            console.log('escuchando puerto 6000');
         });
     });
 
 };
-main();
-mainSales();
-mainUsers();
+
